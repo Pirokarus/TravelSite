131db2695522498e7e2e6b45710e1e0d6d1063cb
Pirokarus <olimjon.kosymoff@yandex.ru>
2017-02-13 15:45:55 +0300

    ComboBox

---
+++ src/com/mySampleApplication/client/MySampleApplication.java
@@ -0,0 +1,51 @@
+package com.mySampleApplication.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>
+ */
+public class MySampleApplication implements EntryPoint {
+
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+
+        final ListBox combobox = new ListBox();
+
+        combobox.addItem("London");
+        combobox.addItem("Rome");
+        combobox.addItem("Paris");
+        combobox.addItem("Berlin");
+        combobox.addItem("Madrid");
+
+        combobox.setSize("120","50");
+
+
+        RootPanel.get("slot1").add(combobox);
+
+    }
+
+    private static class MyAsyncCallback implements AsyncCallback<String> {
+        private Label label;
+
+        public MyAsyncCallback(Label label) {
+            this.label = label;
+        }
+
+        public void onSuccess(String result) {
+            label.getElement().setInnerHTML(result);
+        }
+
+        public void onFailure(Throwable throwable) {
+            label.setText("Failed to receive answer from server!");
+        }
+    }
+}
