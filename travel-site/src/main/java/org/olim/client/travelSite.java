0c7a56a7b42bed966a7d39b2348049e0c6c706f1
Pirokarus <olimjon.kosymoff@yandex.ru>
2017-02-15 00:01:09 +0300

    1 page

--- travel-site/src/main/java/org/olim/client/travelSite.java
+++ travel-site/src/main/java/org/olim/client/travelSite.java
@@ -1,5 +1,7 @@
 package org.olim.client;
 
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.datepicker.client.DateBox;
 import org.olim.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
@@ -9,141 +11,102 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class travelSite implements EntryPoint {
-  /**
-   * The message displayed to the user when the server cannot be reached or
-   * returns an error.
-   */
-  private static final String SERVER_ERROR = "An error occurred while "
-      + "attempting to contact the server. Please check your network "
-      + "connection and try again.";
 
-  /**
-   * Create a remote service proxy to talk to the server-side Greeting service.
-   */
-  private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
-
-  private final Messages messages = GWT.create(Messages.class);
-
-  /**
-   * This is the entry point method.
-   */
   public void onModuleLoad() {
-    final Button sendButton = new Button( messages.sendButton() );
-    final TextBox nameField = new TextBox();
-    nameField.setText( messages.nameField() );
-    final Label errorLabel = new Label();
 
-    // We can add style names to widgets
-    sendButton.addStyleName("sendButton");
+   // final Label MyTravelLabel = new Label("Моё путешествие");
+    final VerticalPanel mainPanel = new VerticalPanel();
+    final HorizontalPanel addPanel = new HorizontalPanel();
+    final Button addButton = new Button("Добавить");
 
-    // Add the nameField and sendButton to the RootPanel
-    // Use RootPanel.get() to get the entire body element
-    RootPanel.get("nameFieldContainer").add(nameField);
-    RootPanel.get("sendButtonContainer").add(sendButton);
-    RootPanel.get("errorLabelContainer").add(errorLabel);
 
-    // Focus the cursor on the name field when the app loads
-    nameField.setFocus(true);
-    nameField.selectAll();
+    //addPanel.add(delButton);
 
-    // Create the popup dialog box
-    final DialogBox dialogBox = new DialogBox();
-    dialogBox.setText("Remote Procedure Call");
-    dialogBox.setAnimationEnabled(true);
-    final Button closeButton = new Button("Close");
-    // We can set the id of a widget by accessing its Element
-    closeButton.getElement().setId("closeButton");
-    final Label textToServerLabel = new Label();
-    final HTML serverResponseLabel = new HTML();
-    VerticalPanel dialogVPanel = new VerticalPanel();
-    dialogVPanel.addStyleName("dialogVPanel");
-    dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-    dialogVPanel.add(textToServerLabel);
-    dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-    dialogVPanel.add(serverResponseLabel);
-    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-    dialogVPanel.add(closeButton);
-    dialogBox.setWidget(dialogVPanel);
+    final ListBox combobox = new ListBox();
 
-    // Add a handler to close the DialogBox
-    closeButton.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {
-        dialogBox.hide();
-        sendButton.setEnabled(true);
-        sendButton.setFocus(true);
-      }
-    });
+    combobox.addItem("London");
+    combobox.addItem("Rome");
+    combobox.addItem("Paris");
+    combobox.addItem("Berlin");
+    combobox.addItem("Madrid");
 
-    // Create a handler for the sendButton and nameField
-    class MyHandler implements ClickHandler, KeyUpHandler {
-      /**
-       * Fired when the user clicks on the sendButton.
-       */
-      public void onClick(ClickEvent event) {
-        sendNameToServer();
-      }
 
-      /**
-       * Fired when the user types in the nameField.
-       */
-      public void onKeyUp(KeyUpEvent event) {
-        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-          sendNameToServer();
-        }
-      }
+    addPanel.add(combobox);
 
-      /**
-       * Send the name from the nameField to the server and wait for a response.
-       */
-      private void sendNameToServer() {
-        // First, we validate the input.
-        errorLabel.setText("");
-        String textToServer = nameField.getText();
-        if (!FieldVerifier.isValidName(textToServer)) {
-          errorLabel.setText("Please enter at least four characters");
-          return;
-        }
+    final DateBox inDate = new DateBox();
 
-        // Then, we send the input to the server.
-        sendButton.setEnabled(false);
-        textToServerLabel.setText(textToServer);
-        serverResponseLabel.setText("");
-        greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-          public void onFailure(Throwable caught) {
-            // Show the RPC error message to the user
-            dialogBox.setText("Remote Procedure Call - Failure");
-            serverResponseLabel.addStyleName("serverResponseLabelError");
-            serverResponseLabel.setHTML(SERVER_ERROR);
-            dialogBox.center();
-            closeButton.setFocus(true);
-          }
+    addPanel.add(inDate);
 
-          public void onSuccess(String result) {
-            dialogBox.setText("Remote Procedure Call");
-            serverResponseLabel.removeStyleName("serverResponseLabelError");
-            serverResponseLabel.setHTML(result);
-            dialogBox.center();
-            closeButton.setFocus(true);
-          }
-        });
-      }
-    }
+    final DateBox outDate = new DateBox();
 
-    // Add a handler to send the name to the server
-    MyHandler handler = new MyHandler();
-    sendButton.addClickHandler(handler);
-    nameField.addKeyUpHandler(handler);
+    addPanel.add(outDate);
+
+
+
+
+    //  RootPanel.get("MyTravelLabelContainer").add(MyTravelLabel);
+    RootPanel.get("MyTravelFieldContainer").add(mainPanel);
+    RootPanel.get("MyAddFieldContainer").add(addPanel);
+    RootPanel.get("MyTravelAddButtonContainer").add(addButton);
+
+
+    addButton.addClickHandler(new ClickHandler() {
+                                @Override
+                                public void onClick(ClickEvent clickEvent) {
+
+                                  final HorizontalPanel sity = new HorizontalPanel();
+                                  final Button delButton = new Button("-");
+                                  sity.add(delButton);
+
+
+                                  final ListBox combobox1 = new ListBox();
+
+                                  combobox1.addItem("London");
+                                  combobox1.addItem("Rome");
+                                  combobox1.addItem("Paris");
+                                  combobox1.addItem("Berlin");
+                                  combobox1.addItem("Madrid");
+
+                                  final DateBox inDate1 = new DateBox();
+
+                                  final DateBox outDate1 = new DateBox();
+
+                                  combobox1.setItemSelected(combobox.getSelectedIndex(),true);
+
+                                  inDate1.setValue(inDate.getValue());
+                                  outDate1.setValue(outDate.getValue());
+
+
+
+
+                                  sity.add(combobox1);
+                                  sity.add(inDate1);
+                                  sity.add(outDate1);
+
+                                  mainPanel.add(sity);
+
+
+                                  delButton.addClickHandler(new ClickHandler() {
+                                    @Override
+                                    public void onClick(ClickEvent clickEvent) {
+
+                                      mainPanel.remove(sity);
+
+                                    }
+                                    });
+
+
+                                }
+                              });
+
+
+
+
+
   }
 }
